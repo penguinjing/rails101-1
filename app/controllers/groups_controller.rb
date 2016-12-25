5c0ff31dbe97f9eed4a76d30107e49a9b73256e3
penguinjing <penguinjing@gmail.com>
2016-12-25 16:20:15 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,10 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def show
+    @group =Group.find(params[:id])
+  end
+
   def create
     @group = Group.new(group_params)
     @group.save
@@ -19,5 +23,5 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:title, :description)
   end
-  
+
 end
