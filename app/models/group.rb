32bd4aa5d615049ce9fdc1ec5eba6d3575358fe6
penguinjing <penguinjing@gmail.com>
2017-01-01 14:36:20 +0800

    implement group relation bug-fix

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,6 @@ class Group < ApplicationRecord
   has_many :posts
   validates :title, presence: true
 
-  has_many :group_relationsships
-  has_many :members, through: :gropu_relationships, source: :user
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226172755) do
+ActiveRecord::Schema.define(version: 20161231154340) do
+
+  create_table "group_relateionships", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_relationships", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
